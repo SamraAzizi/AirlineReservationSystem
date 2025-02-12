9db4546516e19643cca456ee785a50a991fb4af3
Samra <118079600+SamraAzizi@users.noreply.github.com>
2025-02-12 18:39:24 +0500

    airlineSystem

--- main.cpp
+++ main.cpp
@@ -25,7 +25,58 @@ void mainMenu(){
     cout<<"\t\t\t\t\t\t\t\t\t"<<endl
 
     cout<<"\t|\t Press 1 To Add The Customer Details    \t|"<<endl;
-    cout<<"\t|\t Press 2 For Flight Registration    \t"<<endl;
+    cout<<"\t|\t Press 2 For Flight Registration        \t|"<<endl;
+    cout<<"\t|\t Press 3 For Ticket and Charges         \t|"<<endl;
+    cout<<"\t|\t Press 4 To Exit                        \t|"<<endl;
+    cout<<"\t\t\t\t\t\t\t\t\t"<<endl
+    cout<<"\t____________________________________"<<endl;
+
+    cout<<"Enter The Choice : ";
+    cin>>lchoice;
+
+
+    Details d;
+    Registration r;
+    Ticket t;
+
+    switch(lchoice){
+        case 1:
+            {
+                cout<<"____________Customers_____________\n"<<endl;
+                d.information()
+                cout<<"Press Any Key To Go Back Main Menu ";
+                cin>>back;
+
+                if (back == 1){
+                    mainMenu();
+                }else{
+                    mainMenu();
+                }
+                break;
+            }
+
+        case 2:
+            {
+                cout<<"________________Book a Flight Using This System____________\n"<<endl;
+                r.flights();
+                break;
+            }
+
+        case 3:
+            cout<<"___________________GET YOUR TICKET___________________\n"<<endl;
+            t.Bill();
+
+            cout<<"Your Ticket Is Printed, You Can Collect It \n"<<endl;
+            cout<<"Press 1 To Display Your Ticket";
+
+            cin>>back;
+
+                if(back==1){
+                    t.Display
+                }
+    }
+
+
 }
 
 int main(){
